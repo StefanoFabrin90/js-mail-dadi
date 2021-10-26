52e013d87e1f8f289d86f437ccefbe336f1c3958
stef_fab@hotmail.it <stef_fab@hotmail.it>
2021-10-26 15:36:17 +0200

    add 1

--- js/main.js
+++ js/main.js
@@ -1,1 +1,20 @@
-console.log('ciao');
+
+//cerca una mail 
+const mail = ['stef@mail.it','luca@mail.it','giacomo@mail.it'];
+console.log(mail);
+
+//refe
+const inputMail = document.getElementById('mail');
+const btn = document.querySelector('.btn');
+
+//evento 
+btn.addEventListener ('click', function(){
+    //ottenere valore
+    const userMail = inputMail.value.trim();
+    //console.log(userMail);
+
+    if (userMail === '') {
+        alert('Attenzione inserire una mail');
+    } 
+});
+
