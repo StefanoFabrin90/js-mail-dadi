02ecaa5d0ecf5a0c553b9151b4b265492494b200
stef_fab@hotmail.it <stef_fab@hotmail.it>
2021-10-26 16:09:33 +0200

    add 3

--- js/main.js
+++ js/main.js
@@ -21,13 +21,20 @@ btn.addEventListener ('click', function() {
 
         for (let i = 0; i < mail.length; i++) {
             //console.log(mail[i]);
+
             if (mail[i] === userMail) {
                 mailFound = true;
                 break;
             }
         }
-
         console.log(mailFound);
+
+        // output, accesso
+        if (mailFound === true) {
+            document.getElementById('accesso').innerHTML =`Welcome ${userMail}`
+        } else {
+            alert('accesso negato')
+        }
     }
 });
 
